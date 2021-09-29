d81935662cf1d3f7c8eb92246d8e5f27b4777840
Corey Miller <coreymiller454@gmail.com>
2021-09-28 21:57:15 -0400

    add /teapot route

--- app.js
+++ app.js
@@ -5,4 +5,11 @@ app.get("/", (req, res) => {
 	res.send("Hello World 2: Electric Boogaloo");
 });
 
+app.get("/teapot", (req, res) => {
+	res.status(418);
+	res.set({
+		"x-test-thing": "yo mama",
+	});
+});
+
 app.listen(port, () => console.log(`App listening on port ${port}!`));
